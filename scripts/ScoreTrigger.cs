4372aec0569fc61bea54b8c8db8cd1980e0131f9
Knightling23 <49211912+Knightling23@users.noreply.github.com>
2021-09-17 17:13:16 -0400

    Add files via upload

---
+++ scripts/ScoreTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTrigger : MonoBehaviour
+{
+    [SerializeField] private Paddle.Side SideThatScored;
+
+    new AudioSource audio;
+    public AudioClip SoundToPlay;
+    public float Volume;
+
+
+    private void OnTriggerEnter2D(Collider2D Collider)
+    {
+        Ball ball = Collider.GetComponent<Ball>();
+        audio = GetComponent<AudioSource>();//play audio on paddle hit
+
+
+        if (ball)
+        {
+            GameController.instance.Score(SideThatScored);//receives info from game controller method named "Score"
+            audio.PlayOneShot(SoundToPlay, Volume);
+        }
+
+    }
+    
+  
+}
---
+++ scripts/UIManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI LeftScoreText;
+    [SerializeField] private TextMeshProUGUI RightScoreText;
+    [SerializeField] private FadeableUI menuUI;
+    [SerializeField] private FadeableUI gameoverUI;
+    [SerializeField] private TextMeshProUGUI winnerText;
+
+    public void Start()
+    {
+        menuUI.FadeIn(true);
+        gameoverUI.FadeOut(true);
+    }
+
+    public void UpdateScoreText(int leftscore, int rightscore)
+    {
+        LeftScoreText.text = leftscore.ToString();
+        RightScoreText.text = rightscore.ToString();
+
+    }
+
+    public void OnGameStart()
+    {
+        menuUI.FadeOut(false);
+        gameoverUI.FadeOut(false);
+    }
+
+    public void ShowMenu()
+    {
+        gameoverUI.FadeOut(false);
+        menuUI.FadeIn(false);
+    }
+
+    public void ShowGameOver(Paddle.Side side)
+    {
+        gameoverUI.FadeIn(false);
+
+        if (side == Paddle.Side.Left)
+        {
+            winnerText.text = "Player One";
+
+        }
+        else if (side==Paddle.Side.Right)
+        {
+            winnerText.text = "Player Two";
+        }
+
+    }
+}
